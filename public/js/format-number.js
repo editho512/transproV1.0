4e22f4ac45bc64d0c8dfc361bcc67fce8d6bb1db
rodrigue <rodrigue@MacBook-Pro-de-Hamza.local>
2022-03-22 15:15:30 +0300

    Ajout de la badge de notification dans la partie camion et formattage des montant

--- public/js/format-number.js
+++ public/js/format-number.js
@@ -1,7 +1,7 @@
 function formatNumber(number, decimal = 2, unite = null, separateur = " ") {
     let  value = (number).toLocaleString("en-US",{
             minimumFractionDigits: decimal
-    }).replace(",", separateur);
+    }).replaceAll(",", separateur);
 
     if (unite !== null) return value + " " + unite
     return value
